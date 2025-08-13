83c5a6a78c80fc73d889ea6bbd577135e147e24c
Lor-77 <barreiroloren@gmail.com>
2025-08-13 17:18:51 -0500

    Subir taller 3

---
+++ taller3.js
@@ -0,0 +1,80 @@
+function mostrar(dato){
+    switch(dato) {
+        case 1:
+         document.getElementById("Ejercicio1").style.display="block";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 2:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="block";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 3:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="block";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 4:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="block";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 5:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="block";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 6:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="block";
+         document.getElementById("Ejercicio7").style.display="none";
+        break;
+        case 7:
+         document.getElementById("Ejercicio1").style.display="none";
+         document.getElementById("Ejercicio2").style.display="none";
+         document.getElementById("Ejercicio3").style.display="none";
+         document.getElementById("Ejercicio4").style.display="none";
+         document.getElementById("Ejercicio5").style.display="none";
+         document.getElementById("Ejercicio6").style.display="none";
+         document.getElementById("Ejercicio7").style.display="block";
+        break;
+
+
+
+
+
+
+
+}}
+
+function Eje1(){
+let números=[9,8,7,6,5];
+console.log(números);
+document.getElementById(resultado).innerHTML=" números"
+
+}             
